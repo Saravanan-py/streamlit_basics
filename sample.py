59a5cc053450915b6ec2c983c919a1bd6660b507
SaravananS30 <saravanan@vrdella.com>
2024-06-21 14:30:42 +0530

    added

--- sample.py
+++ sample.py
@@ -1,8 +1,7 @@
 import streamlit as st
 
 st.title("Sample title")
-text = st.text_input("Enter your key: ")
-text1 = st.text_input("Enter your value: ")
-if text and text1:
-    st.session_state[text] = text1
-    st.write(st.session_state)
+prompt = st.chat_input("ask for anything...")
+if prompt:
+    with st.chat_message('assistant'):
+        st.markdown(prompt)
